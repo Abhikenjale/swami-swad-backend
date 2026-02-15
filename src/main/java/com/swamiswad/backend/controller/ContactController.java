20cc4483572f4cf10c7e688a3bb7c1115871f8c4
Abhishek Kenjale <abhishekkenjale101@gmail.com>
2026-02-15 16:05:24 +0530

    backend

---
+++ src/main/java/com/swamiswad/backend/controller/ContactController.java
@@ -0,0 +1,53 @@
+package com.swamiswad.backend.controller;
+
+import com.swamiswad.backend.model.Contact;
+import com.swamiswad.backend.repository.ContactRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/contact")
+//@CrossOrigin(origins = "*")
+public class ContactController {
+
+    private final ContactRepository contactRepository;
+
+    public ContactController(ContactRepository contactRepository) {
+        this.contactRepository = contactRepository;
+    }
+
+    @PostMapping
+    public Contact saveContact(@RequestBody Contact contact) {
+        return contactRepository.save(contact);
+    }
+
+
+    @GetMapping
+    public List<Contact> getAllContacts() {
+        return contactRepository.findAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteContact(@PathVariable Long id) {
+        contactRepository.deleteById(id);
+        return "Deleted successfully!";
+    }
+
+    @PutMapping("/{id}")
+    public Contact updateContact(@PathVariable Long id, @RequestBody Contact updatedContact) {
+
+        Contact existingContact = contactRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Contact not found"));
+
+        existingContact.setName(updatedContact.getName());
+        existingContact.setMobile(updatedContact.getMobile());
+        existingContact.setMessage(updatedContact.getMessage());
+
+        return contactRepository.save(existingContact);
+    }
+
+
+
+}
---
+++ src/main/java/com/swamiswad/backend/model/Contact.java
@@ -0,0 +1,45 @@
+package com.swamiswad.backend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String mobile;
+    private String message;
+
+    public Contact() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
